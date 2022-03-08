8bf2f7c77a6282b7745f8f8bd56d2551c3ac45cf
M Natoli <natoli.missy@gmail.com>
2022-03-08 15:04:25 -0600

    starting seasons app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        //this creates the initial state
+        this.state = { lat: null, errorMessage: '' };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we call setState to update our state object
+                this.setState({ lat: position.coords.latitude });
+            }, 
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    // React says we have to define render
+    render() { 
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        } 
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading...</div>
+    }
+}
+
+ReactDom.render(<App />, document.querySelector("#root"));
